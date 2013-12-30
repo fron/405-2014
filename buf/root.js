fb850b0798bb42fbd54af3cf17918997513b7f0c
Francisco Ron <francisqueins@hotmail.com>
2013-12-29 18:08:52 -0800

    buf/root.js correctly indented

--- buf/root.js
+++ buf/root.js
@@ -1,18 +1,9 @@
 // program root for buffers assignment
 
 var http = require('http');
-
 var body = new Buffer("I'm a codfish.", 14);
 
 exports.handle = function(req, res) {
-    res.writeHead(200, {
-		
-        'Content-Type': 'text/plain', 
-		'Content-Length': 14
-    }
-	
-	
-	
-	);
+    res.writeHead(200, {'Content-Type': 'text/plain; whatever:whatever', 'Content-Length': 14});
     res.end(body);
 };
