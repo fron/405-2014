d054291122e8a290eab7b17ff858ccb160f1a835
Francisco Ron <francisqueins@hotmail.com>
2013-12-29 23:20:30 -0800

    jsonx assignment completed

---
+++ json/image.js
@@ -0,0 +1,21 @@
+// program image.js for json assignment
+
+var http = require('http');
+var body;
+
+exports.handle = function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'image.png; charset=UTF-8',
+        'Content-Length': body.length
+    });
+    res.end(body);
+};
+
+   
+exports.init = function(cb) {
+    require('fs').readFile('./image.png', function(err, data) {
+        if (err) throw err;
+        body = data;
+        cb();
+    });
+}
---
+++ json/message.js
@@ -0,0 +1,16 @@
+// program message for json assignment
+// it sends a message 
+
+var http = require('http');
+var responseObject = { msg: 'Hello JSON.' };
+var responseString = JSON.stringify(responseObject);
+var body = new Buffer(responseString, 'utf-8');
+
+
+exports.handle = function(req, res) {
+    res.writeHead(200, {
+	    'Content-Type': 'application/json',
+		'Content-length':body.length
+	});
+    res.end(body);
+};
