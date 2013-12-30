1e9f9bda5cb8ecd9ddb31cdd4d7e86a33b75c383
Francisco Ron <francisqueins@hotmail.com>
2013-12-29 19:16:13 -0800

    ajax assignment correctly indented

--- ajax/image.js
+++ ajax/image.js
@@ -1,20 +1,21 @@
-var http = require('http');
+// program image.js for ajax assignment
 
+var http = require('http');
 var body;
 
 exports.handle = function(req, res) {
-  res.writeHead(200, {
-      'Content-Type': 'image.png; charset=UTF-8',
-      'Content-Length': body.length
-  });
-  res.end(body);
+    res.writeHead(200, {
+        'Content-Type': 'image.png; charset=UTF-8',
+        'Content-Length': body.length
+    });
+    res.end(body);
 };
 
    
 exports.init = function(cb) {
-   require('fs').readFile('./image.png', function(err, data) {
-   if (err) throw err;
-   body = data;
-   cb();
-  });
+    require('fs').readFile('./image.png', function(err, data) {
+        if (err) throw err;
+        body = data;
+        cb();
+    });
 }
--- ajax/message.js
+++ ajax/message.js
@@ -1,18 +1,10 @@
-// program root for ajax assignment
+// program message for ajax assignment
+// it sends a message 
 
 var http = require('http');
-
 var body = new Buffer("Hello Ajax.", 11);
 
 exports.handle = function(req, res) {
-    res.writeHead(200, {
-		
-        'Content-Type': 'text/plain; whatever:whatever', 
-		'Content-Length': 11
-    }
-	
-	
-	
-	);
+    res.writeHead(200, {'Content-Type': 'text/plain; whatever:whatever', 'Content-Length': 11});
     res.end(body);
 };
