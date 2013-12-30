5834fb1677d4167c09e1ba2093a5999738467d87
Francisco Ron <francisqueins@hotmail.com>
2013-12-29 18:37:12 -0800

    file assignment correctly indented

--- file/main.js
+++ file/main.js
@@ -4,43 +4,46 @@ var root = require('./root');
 var image = require('./image');
 
 function replyError(res) {
-  try {
-    res.writeHead(500);
-    res.end('Server error.');
-  } catch (err) {
-    console.error('Error sending response with code 500.');
-  }
+    try {
+        res.writeHead(500);
+        res.end('Server error.');
+    } 
+   
+    catch (err) {
+        console.error('Error sending response with code 500.');
+    }
 };
 
+
 function replyNotFound(res) {
-  res.writeHead(404);
-  res.end('not found');
+    res.writeHead(404);
+    res.end('not found');
 }
 
 function handleRequest(req, res) {
-  console.log('Handling request for ' + req.url);
-  switch (true) {
-      case (req.url === '/image.png'):
-          image.handle(req, res);
-		  break;
-	  case (req.url === '/'):
-	      root.handle(req, res);
-		  break; 
-       default:
-          replyNotFound(res);
-		  break
-  }
+    console.log('Handling request for ' + req.url);
+    switch (true) {
+        case (req.url === '/image.png'):
+            image.handle(req, res);
+		    break;
+	    case (req.url === '/'):
+	        root.handle(req, res);
+		    break; 
+        default:
+            replyNotFound(res);
+		    break
+    }
 }
 
 var server = http.createServer();
 
 server.on('request', function(req, res) {
-  var d = domain.create();
-  d.on('error', function(err) {
-      console.error(req.url, err.message);
-      replyError(res);
-  });
-  d.run(function() { handleRequest(req, res); });
+    var d = domain.create(); 
+	d.on('error', function(err) {
+        console.error(req.url, err.message);
+        replyError(res);
+    });
+    d.run(function() { handleRequest(req, res); });
 });
 
 var ok1 = false;
@@ -65,7 +68,7 @@ function callback1() {
     root.init(function(){
         ok1 = true;
 		f(callback1);
-        });
+    });
 }
 
 f(callback1);
