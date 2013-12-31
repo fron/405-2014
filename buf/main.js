47238a63b5716e160e77b4438b81e7c79fc26adf
Francisco Ron <francisqueins@hotmail.com>
2013-12-31 12:05:07 -0800

    buf assignment content-type modified

--- buf/main.js
+++ buf/main.js
@@ -1,43 +1,41 @@
 // program main.js for buffers assignment
 
-
 var http = require('http');
 var domain = require('domain');
 var root = require('./root');
+var server = http.createServer();
 
-function replyError(res) {
-   try {
-       res.writeHead(500);
-       res.end('Server error.');
-   }    
-   catch (err) {
-       console.error('Error sending response with code 500.');
-   }
+function replyError(res){
+    try{
+        res.writeHead(500);
+        res.end('Server error.');
+    }    
+    catch(err){
+        console.error('Error sending response with code 500.');
+    }
 };
 
-function replyNotFound(res) {
+function replyNotFound(res){
     res.writeHead(404);
     res.end('not found');
 }
 
-function handleRequest(req, res) {
+function handleRequest(req, res){
     console.log('Handling request for ' + req.url);
-    if (req.url === '/') {
+    if (req.url === '/'){
         root.handle(req, res);
-    } else {
-        replyNotFound(res);
+    }else{
+         replyNotFound(res);
     }
 }
 
-var server = http.createServer();
-
-server.on('request', function(req, res) {
+server.on('request', function(req, res){
     var d = domain.create();
-    d.on('error', function(err) {
+    d.on('error', function(err){
         console.error(req.url, err.message);
         replyError(res);
     });
-    d.run(function() { handleRequest(req, res); });
+    d.run(function() {handleRequest(req, res);});
 });
 
 server.listen(5000);
--- buf/root.js
+++ buf/root.js
@@ -1,9 +1,9 @@
 // program root for buffers assignment
 
 var http = require('http');
-var body = new Buffer("I'm a codfish.", 14);
+var body = new Buffer("I'm a codfish.話", 'utf-8');
 
 exports.handle = function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain; whatever:whatever', 'Content-Length': 14});
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8', 'Content-Length': body.length});
     res.end(body);
 };
