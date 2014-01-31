e63263a39e3a6931a0033db7c1b3ed918ff20c16
Francisco Ron <francisqueins@hotmail.com>
2014-01-30 19:52:38 -0800

    remote.bat modified

--- heroku/scripts/remote.bat
+++ heroku/scripts/remote.bat
@@ -1,3 +1,3 @@
-psql -h localhost -d gift -f gift.sql
+psql -U postgres -h localhost -d gift -f gift.sql
 heroku pg:reset DATABASE_URL --app fr405app --confirm fr405app
 heroku pg:push gift DATABASE_URL --app fr405app
