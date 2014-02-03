d9607918a8b17618e25552edd862511d4ccdb479
Francisco Ron <francisqueins@hotmail.com>
2014-02-03 11:09:15 -0800

    .git ignore moved from /mobile to /405

--- ajax/message.js
+++ ajax/message.js
@@ -5,6 +5,6 @@ var http = require('http');
 var body = new Buffer("Hello Ajax.", 11);
 
 exports.handle = function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain; whatever:whatever', 'Content-Length': 11});
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=UTF-8', 'Content-Length': 11});
   res.end(body);
 };
